cbe4c832408842b1a13caa310031e55b71cde147
Mikhail Gorshkov <mikhail.gorshkov@gmail.com>
2021-08-21 12:41:24 +0300

    Initial Commit

---
+++ luxoft_test_task/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  luxoft_test_task
+//
+//  Created by Mikhail Gorshkov on 21.08.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
